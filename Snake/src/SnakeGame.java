92a7dcd0efb9da900b194f07ef4a8fbbfad149af
GjorgjievaS <gorgieva.sg@gmail.com>
2022-01-29 22:51:53 +0100

    game in java

---
+++ Snake/src/SnakeGame.java
@@ -0,0 +1,10 @@
+public class SnakeGame {
+
+    public static void main(String[] args) {
+
+        GameFrame frame = new GameFrame();
+
+        //if we dont use this frame for anything else
+       // new GameFrame();
+    }
+}
